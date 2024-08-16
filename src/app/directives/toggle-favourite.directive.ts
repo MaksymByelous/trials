75d173d330d42d59095ff5b186b3e4c5983ad579
maksymbyelous <maxwellbels@gmail.com>
2024-08-16 14:15:29 +0200

    toggle favs directive: fix types and deps

--- src/app/directives/toggle-favourite.directive.ts
+++ src/app/directives/toggle-favourite.directive.ts
@@ -1,13 +1,13 @@
+import { HitId } from '../models/study';
 import { FavouritesService } from '../services/favourites.service';
 import { Directive, HostListener, inject, Input } from '@angular/core';
 
 @Directive({
   selector: '[toggleFavourite]',
-  providers: [FavouritesService],
   standalone: true,
 })
 export class ToggleFavouriteDirective {
-  @Input({ required: true, alias: 'toggleFavourite' }) favouriteId!: string;
+  @Input({ required: true, alias: 'toggleFavourite' }) favouriteId!: HitId;
   private favouritesService = inject(FavouritesService);
 
   @HostListener('click') toggleFavourite(): void {
